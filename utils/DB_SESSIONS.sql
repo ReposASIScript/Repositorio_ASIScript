cc7ec292d91f5b88214a6d31d4770c3c54e95fa9
Gilberto Guerrero <gilberto.guerrero@grupo-asi.com>
2019-01-10 16:06:20 -0500

    Para actualizar repositorio

---
+++ utils/DB_SESSIONS.sql
@@ -0,0 +1,4 @@
+  SELECT ver.SID, ver.SERIAL#, ver.USERNAME, ver.PROGRAM, ver.STATUS, ver.SCHEMANAME, ver.OSUSER, ver.MACHINE
+    FROM v$session ver
+   WHERE ver.USERNAME = NVL('&Introduzca_Usuario',ver.USERNAME)
+ORDER BY 1, 2;
